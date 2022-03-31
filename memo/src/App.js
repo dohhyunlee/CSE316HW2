5cda127b9de9d24e24eb0ae159d3d9d9d2b03824
dohhyunlee <dohhyun.lee@stonybrook.edu>
2022-04-01 02:10:50 +0900

    memo_03 (req2 complete)

--- memo/src/App.js
+++ memo/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {useRef} from "react";
+import Tag from './components/Tag';
 import './app.css';
 
 
@@ -58,19 +59,22 @@ function App() {
         id : 0,
         text : "CSE316",
         date : getTime(),
-        exist : 1
+        exist : 1,
+        tags: []
     };
     const secondn = {
         id : 1,
         text : "Another wrapping line example",
         date : getTime(),
-        exist : 1
+        exist : 1,
+        tags: []
     };
     const thirdn = {
         id : 2,
         text : "This is a note with a long line of text. Notice that the text will automatically wrap to the next line once it reaches the right side of the screen.\n\nYou can press enter to add new lines as well.",
         date : getTime(),
-        exist : 1
+        exist : 1,
+        tags: []
     };
 
     const initArray = [firstn,secondn,thirdn];
@@ -83,16 +87,18 @@ function App() {
 
     const [input, setInput] = useState(currentNote.text);
 
-    const [name, setName] = useState("Simpson");
+    const [date, setDate] = useState(new Date());
 
-    const [email, setEmail] = useState("simpson@stonybrook.edu");
+    const getNote = () => {
+
+    }
 
     const deleteNote = () => {
-        const newArray = noteArray.filter(note => note.id !== currentNote.id)
-        setNoteArray(newArray);
-        const newCurnote = newArray[newArray.length-1];
-        setcurrentNote(newCurnote);
-        setInput(newCurnote.text);
+        currentNote.text = "";
+        currentNote.exist = 0;
+        setNoteArray(noteArray.filter(note => note.id !== currentNote.id));
+        setcurrentNote(noteArray[noteArray.length-1]);
+        inputRef.current.value = currentNote.text;
     }
 
     const getFirstline = (note) =>{
@@ -107,67 +113,70 @@ function App() {
             id : noteID,
             text : "New Note",
             date : getTime(),
-            exist : 1
+            exist : 1,
+            tags: []
         }
         setcurrentNote(newNote);
         setNoteArray(noteArray.concat(newNote));
-        setInput("New Note");
+        inputRef.current.value = "";
     }
 
     const update = () => {
-        currentNote.text = input;
+        currentNote.text = inputRef.current.value;
         currentNote.date = getTime();
     }
 
     return (
         <div>
-            <div className="rows">
-                <div className="row">
-                    <div className='logo'>
-                        <img src="./simpson.jpg" className="profile"
-                             onClick={openModal} alt="MyImage"></img>
-                        <div className="title">My Notes</div>
-                        <span className="material-icons" id="noteadd" onClick={insertNote}
-                              style={{float:'right', top:'6px'}}>note_add</span>
-                    </div>
-                    <div className="main">
-                        <span className="material-icons" style={{left:"20px"}}>notification_add</span>
-                        <span className="material-icons" style={{left:"45%"}}>person_add_alt</span>
-                        <span className="material-icons" onClick={deleteNote}
-                              style={{float:"right"}}>delete_outline</span>
-                    </div>
-                    <div className="main2">
+        <div className="rows">
+            <div className="row">
+                <div className='logo'>
+                    <img src="./simpson.jpg" className="profile"
+                         onClick={openModal} alt="MyImage"></img>
+                    <div className="title">My Notes</div>
+                    <span className="material-icons" id="noteadd" onClick={insertNote}
+                          style={{float:'right', top:'6px'}}>note_add</span>
+                </div>
+                <div className="main">
+                    <span className="material-icons" style={{left:"20px"}}>notification_add</span>
+                    <span className="material-icons" style={{left:"45%"}}>person_add_alt</span>
+                    <span className="material-icons" onClick={deleteNote}
+                          style={{float:"right"}}>delete_outline</span>
+                </div>
+                <div className="main2">
                         <span className="material-icons" onClick="document.getElementById('id01').style.display='block'"
                               style={{left:"20px"}}>arrow_back</span>
-                        <span className="material-icons" style={{left:"25%"}}>notification_add</span>
-                        <span className="material-icons" style={{left:"55%"}}>person_add_alt</span>
-                        <span className="material-icons" onClick={deleteNote}
-                              style={{float:"right"}}>delete_outline</span>
-                    </div>
-                </div>
-                <div className="row2">
-                    <div className="sidebar">
-                        <div className="searchbar">
-                            <span className="material-icons" style={{left:"20px"}}>search</span>
-                            <input className="notesearch" type="search" placeholder="Search all notes"></input>
-                        </div>
-                        <div className="noteList">
-                            {noteArray.map((note) => (
-                                <div className={`listed ${currentNote === note && "on"}`} onClick={() => {setInput(note.text);setcurrentNote(note) }}>
-                                    <div className="note">{getFirstline(note)}
-                                        <div className="curdate">{note.date}</div>
-                                    </div>
-                                </div>
-                            ))}
-
-                        </div>
-                    </div>
-                    <div className="text">
-                        <textarea className="textfield" onKeyUp={update} value={input} onChange={e => setInput(e.target.value)}
-                                  style={{height:"99%", width:"99%", border:"none", resize: "none"}}></textarea>
-                    </div>
+                    <span className="material-icons" style={{left:"25%"}}>notification_add</span>
+                    <span className="material-icons" style={{left:"55%"}}>person_add_alt</span>
+                    <span className="material-icons" onClick={deleteNote}
+                          style={{float:"right"}}>delete_outline</span>
                 </div>
             </div>
+            <div className="row2">
+                <div className="sidebar">
+                    <div className="searchbar">
+                        <span className="material-icons" style={{left:"20px"}}>search</span>
+                        <input className="notesearch" type="search" placeholder="Search all notes"></input>
+                    </div>
+                    <div className="noteList">
+                        {noteArray.map((note) => (
+                            <div className={`listed ${currentNote === note && "on"}`} onClick={() => {inputRef.current.value = note.text;setcurrentNote(note) }}>
+                                <div className="note">{getFirstline(note)}
+                                    <div className="curdate">{note.date}</div>
+                                </div>
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+                <div className="text">
+                    <textarea className="textfield" ref={inputRef} onKeyUp={update}
+                              style={{height:"100%", width:"100%",border:"none", resize: "none"}}></textarea>
+                    <Tag className="tagfield" note={currentNote}
+                         />
+                </div>
+            </div>
+        </div>
             <div id="id01" className={modalOpen ? 'modal' : 'closemodal'} onClick={(e) => ModalOff(e)}>
                 <form className="modal-content" action="" ref={modalRef}>
                     <div className="container">
@@ -182,10 +191,10 @@ function App() {
                             <div className="removeimg">Remove image</div>
                         </div>
                         <label htmlFor="name"><b>Name</b></label>
-                        <input type="text" value={name} placeholder="Enter Name" name="name" onChange={e => setName(e.target.value)} required/>
+                        <input type="text" value="Dohhyun" placeholder="Enter Name" name="name" required/>
 
                         <label htmlFor="email"><b>Email</b></label>
-                        <input type="text" value={email} placeholder="Enter Email" name="email" onChange={e => setEmail(e.target.value)}
+                        <input type="text" value="dohhyun.lee@stonybrook.edu" placeholder="Enter Email" name="email"
                                required/>
 
                         <label htmlFor="psw-repeat"><b>Color Scheme</b></label>
